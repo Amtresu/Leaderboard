72fe45df6e7f7c33667b3a43e0622cb7ce3c2491
Amtresu <conor.follette@gmail.com>
2022-08-23 17:26:47 +0900

    fixing linter errors

--- src/mods/insertObj.js
+++ src/mods/insertObj.js
@@ -1,8 +1,7 @@
-
 const scoreList = document.querySelector('.item-list')
 
 export const insertObject = (score) => {
-    score.forEach((score) => {
-      scoreList.innerHTML += `<li><p class="player">${score.name} : ${score.score}</p></li>`;
-    });
-  };
+  score.forEach((score) => {
+    scoreList.innerHTML += `<li><p class="player">${score.name} : ${score.score}</p></li>`
+  })
+}
