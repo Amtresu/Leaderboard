c7755cb28ab3025f5e102b86ad795bcdd5a24ebe
Amtresu <conor.follette@gmail.com>
2022-08-23 17:10:40 +0900

    adding dependency files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import _ from 'lodash';
-import './style.css';
+import './style.css'
+import { insertObject } from './mods/insertObj'
 
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-     element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const scores = [ {
+    name: 'dave',
+    score: '5'
+  },
+  {
+    name: 'Juan',
+    score: '10'
+  }]
+
+const submitButton = document.querySelector('.submit-button')
+const itemList = document.querySelector('.item-list')
+
+submitButton.addEventListener('click', () =>{
+itemList.style.visibility = 'visible'
+insertObject(scores)
+})
