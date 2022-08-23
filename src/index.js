e0faa76e6b2ae71bf3da7a6b160e38123092931b
Amtresu <conor.follette@gmail.com>
2022-08-23 17:22:10 +0900

    fixing linter errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './style.css'
 import { insertObject } from './mods/insertObj'
 
-const scores = [ {
-    name: 'dave',
-    score: '5'
-  },
-  {
-    name: 'Juan',
-    score: '10'
-  }]
+const scores = [{
+  name: 'dave',
+  score: '5'
+},
+{
+  name: 'Juan',
+  score: '10'
+}]
 
 const submitButton = document.querySelector('.submit-button')
 const itemList = document.querySelector('.item-list')
 
-submitButton.addEventListener('click', () =>{
-itemList.style.visibility = 'visible'
-insertObject(scores)
+submitButton.addEventListener('click', () => {
+  itemList.style.visibility = 'visible'
+  insertObject(scores)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
-    static: './dist',
+    static: './dist'
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-    }),
+      template: './src/index.html'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    clean: true
   },
 
   optimization: {
-    runtimeChunk: 'single',
-  },
- };
-
+    runtimeChunk: 'single'
+  }
+}
