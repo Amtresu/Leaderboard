b61b67f55b667a10e9d73f553ca16ef1a8a2da48
Amtresu <conor.follette@gmail.com>
2022-08-24 23:55:11 +0900

    adding API functions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import './style.css'
-import { insertObject } from './mods/insertObj'
-
-const scores = [{
-  name: 'dave',
-  score: '5'
-},
-{
-  name: 'Juan',
-  score: '10'
-}]
+// import { insertObject} from './mods/insertObj'
+import { addItems, getData } from './mods/api'
 
 const submitButton = document.querySelector('.submit-button')
-const itemList = document.querySelector('.item-list')
+const form = document.querySelector('.form-section')
+const name = document.querySelector('.name-input')
+const score = document.querySelector('.score-input')
+const refreshButton = document.querySelector('.refresh-button')
+const scoreList = document.querySelector('.item-list')
 
-submitButton.addEventListener('click', () => {
-  itemList.style.visibility = 'visible'
-  insertObject(scores)
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  addItems(name, score)
+  form.reset()
+})
+
+refreshButton.addEventListener('click', () => {
+  insertList()
+})
+
+const buildList = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    const item = `<li>${data[i].user} : ${data[i].score}</li>`
+    scoreList.innerHTML += item
+  }
+}
+
+const insertList = async () => {
+  const getResult = await getData()
+  getResult.forEach((result) => {
+    buildList(result)
+    console.log(result)
+    console.log(buildList(result))
+  })
+}
+
+window.addEventListener('load', () => {
 })
